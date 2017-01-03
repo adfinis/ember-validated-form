1d646814c61ea0a64400e9a9b78b7280882060ab
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2017-01-03 10:13:08 +0100

    Add basic tests

--- tests/integration/components/validated-form/component-test.js
+++ tests/integration/components/validated-form/component-test.js
@@ -5,21 +5,40 @@ moduleForComponent('validated-form', 'Integration | Component | validated form',
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{validated-form}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+test('it renders simple inputs', function(assert) {
   this.render(hbs`
-    {{#validated-form}}
-      template block text
+    {{#validated-form as |f|}}
+      {{f.input label="First name"}}
     {{/validated-form}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('form label').text().trim(), 'First name');
+  assert.equal(this.$('form input').attr('type'), 'text');
+});
+
+test('it renders textareas', function(assert) {
+  this.render(hbs`
+    {{#validated-form as |f|}}
+      {{f.input type="textarea" label="my label"}}
+    {{/validated-form}}
+  `);
+
+  assert.equal(this.$('form label').text().trim(), 'my label');
+  assert.equal(this.$('form textarea').length, 1);
+});
+
+test('it renders submit buttons', function(assert) {
+  this.on('submit', function() {});
+
+  this.render(hbs`
+    {{#validated-form
+      on-submit=(action "submit")
+      submit-label= 'Save'
+      as |f|}}
+      {{f.input label="First name"}}
+    {{/validated-form}}
+  `);
+
+  assert.equal(this.$('form button').attr('type'), 'submit');
+  assert.equal(this.$('form button').text().trim(), 'Save');
 });
