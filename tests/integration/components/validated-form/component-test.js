614cd7fc0f8c241b2ce2a76115f18e92d04447f8
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2017-01-03 11:29:21 +0100

    Add validation tests

--- tests/integration/components/validated-form/component-test.js
+++ tests/integration/components/validated-form/component-test.js
@@ -1,5 +1,7 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import UserValidations from 'dummy/validations/user';
 
 moduleForComponent('validated-form', 'Integration | Component | validated form', {
   integration: true
@@ -33,12 +35,63 @@ test('it renders submit buttons', function(assert) {
   this.render(hbs`
     {{#validated-form
       on-submit=(action "submit")
-      submit-label= 'Save'
-      as |f|}}
+      submit-label='Save' as |f|}}
       {{f.input label="First name"}}
     {{/validated-form}}
   `);
 
   assert.equal(this.$('form button').attr('type'), 'submit');
   assert.equal(this.$('form button').text().trim(), 'Save');
+});
+
+test('it performs basic validations on submit', function(assert) {
+  this.on('submit', function() {});
+  this.set('UserValidations', UserValidations);
+
+  const store = this.container.lookup('service:store');
+  Ember.run(() => {
+    this.set('model', store.createRecord('user', {
+      firstName: 'x'
+    }));
+  });
+
+  this.render(hbs`
+    {{#validated-form
+      model=(changeset model UserValidations)
+      on-submit=(action "submit")
+      submit-label='Save' as |f|}}
+      {{f.input label="First name" name="firstName"}}
+    {{/validated-form}}
+  `);
+  assert.equal(this.$('span.help-block').length, 0);
+  this.$('button').click();
+  assert.equal(this.$('input').val(), 'x');
+  assert.equal(this.$('span.help-block').length, 1);
+  assert.equal(this.$('span.help-block').text(),
+    'First name must be between 3 and 40 characters');
+});
+
+test('it performs basic validations on focus out', function(assert) {
+  this.on('submit', function() {});
+  this.set('UserValidations', UserValidations);
+
+  const store = this.container.lookup('service:store');
+  Ember.run(() => {
+    this.set('model', store.createRecord('user'));
+  });
+
+  this.render(hbs`
+    {{#validated-form
+      model=(changeset model UserValidations)
+      on-submit=(action "submit")
+      submit-label='Save' as |f|}}
+      {{f.input label="First name" name="firstName"}}
+    {{/validated-form}}
+  `);
+  assert.equal(this.$('span.help-block').length, 0);
+  this.$('input').blur();
+
+  assert.equal(this.$('span.help-block').length, 1);
+  assert.equal(this.$('span.help-block').text(),
+    'First name can\'t be blank');
 });
