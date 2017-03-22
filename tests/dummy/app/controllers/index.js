9f08828542c8d74f863fa6960e03d4d5d7b8c956
feanor07 <alptugd@gmail.com>
2017-03-22 23:28:20 +0300

    added support for block-form usage of validated-input so as to better support custom component usage

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -5,6 +5,7 @@ import { task, timeout } from 'ember-concurrency';
 export default Ember.Controller.extend({
   UserValidations,
 
+  colors:[{name:'Red', color:'red'}, {name:'Green', color:'green'}, {name: 'Blue', color:'blue'}],
   countries: ['United States', 'United Kingdom', 'Switzerland', 'Other'],
   genders: [{
     key: 'm',
--- tests/dummy/app/validations/user.js
+++ tests/dummy/app/validations/user.js
@@ -4,6 +4,21 @@ import {
 } from 'ember-changeset-validations/validators';
 
 
+function validateTerm() {
+  return (key, newValue) => {
+    // validation logic
+    // return `true` if valid || error message string if invalid
+    let result = newValue === true;
+
+    if (!result) {
+      return "Terms must be accepted!";
+    }
+
+    return true;
+  };
+}
+
+
 export default {
   firstName: [
     validatePresence(true),
@@ -15,5 +30,7 @@ export default {
   ],
   aboutMe: [ validateLength({allowBlank: true, max: 200}) ],
   country: [ validatePresence(true) ],
-  gender: [ validatePresence(true) ]
+  gender: [ validatePresence(true) ],
+  accepTheTerms: [ validateTerm() ],
+  color: [validatePresence(true)]
 };
