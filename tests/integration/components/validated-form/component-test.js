ccd7d8d19dc04442c478b0619eb4329fc258399f
Jacob Niedzwiecki <jniedzwiecki@financeit.io>
2017-05-24 14:40:40 -0400

    Added test for radioGroup block form (using stubbed ember-i18n 't' helper)

--- tests/integration/components/validated-form/component-test.js
+++ tests/integration/components/validated-form/component-test.js
@@ -5,6 +5,9 @@ import UserValidations from 'dummy/validations/user';
 
 moduleForComponent('validated-form', 'Integration | Component | validated form', {
   integration: true
+  // beforeEach: function(){
+
+  // }
 });
 
 test('it renders simple inputs', function(assert) {
@@ -62,18 +65,62 @@ test('it renders a radio group with block form', function(assert) {
 
   this.render(hbs`
     {{#validated-form as |f|}}
-      {{#f.input type='radioGroup' label='Options' name='testOptions' options=buttonGroupData.options}}
-        Option - block form
+      {{#f.input type='radioGroup' label='Options' name='testOptions' options=buttonGroupData.options as |option|}}
+        {{option.label}} - block form
       {{/f.input}}
     {{/validated-form}}
   `);
 
-  console.log(this.$());
   assert.equal(this.$('input[type="radio"]').length, 3);
   assert.equal(this.$('label').eq(0).text().trim(), 'Options');
-  assert.equal(this.$('label').eq(1).text().trim(), 'Option - block form');
-  assert.equal(this.$('label').eq(2).text().trim(), 'Option - block form');
-  assert.equal(this.$('label').eq(3).text().trim(), 'Option - block form');
+  assert.equal(this.$('label').eq(1).text().trim(), 'Option 1 - block form');
+  assert.equal(this.$('label').eq(2).text().trim(), 'Option 2 - block form');
+  assert.equal(this.$('label').eq(3).text().trim(), 'Option 3 - block form');
+});
+
+test('it renders a radio group with block form and i18n support', function(assert) {
+  // this.register('service:i18n', i18nStub);
+  // this.inject.service('i18n', { as: 'i18n'}).then(function(){
+  //     this.container.lookup('service:i18n').set('locale', 'en');
+  //     // this.registry.register('helper:t', tHelper);
+  //   });
+  this.container.registry.registrations['helper:t'] = Ember.Helper.helper(function(arg){
+    const key = arg[0];
+    switch(key) {
+      case 'label.foo':
+        return 'Option One';        
+      case 'label.bar':
+        return 'Option Two';
+      case 'label.baz':
+        return 'Option Three';  
+      default:
+        return false;   
+    } 
+  });
+
+  this.set('buttonGroupData', {
+    options: [
+      { key: '1', label: 'label.foo'},
+      { key: '2', label: 'label.bar'},
+      { key: '3', label: 'label.baz'},
+    ]
+  });
+
+  this.render(hbs`
+    {{#validated-form as |f|}}
+      {{#f.input type='radioGroup' label='Options' name='testOptions' options=buttonGroupData.options as |option|}}
+        {{t option.label}} - block form
+      {{/f.input}}
+    {{/validated-form}}
+  `);
+
+  assert.equal(this.$('input[type="radio"]').length, 3);
+  assert.equal(this.$('label').eq(0).text().trim(), 'Options');
+  assert.equal(this.$('label').eq(1).text().trim(), 'Option One - block form');
+  assert.equal(this.$('label').eq(2).text().trim(), 'Option Two - block form');
+  assert.equal(this.$('label').eq(3).text().trim(), 'Option Three - block form');
+
+  this.container.registry.registrations['helper:t'] = null;
 });
 
 test('it renders submit buttons', function(assert) {
