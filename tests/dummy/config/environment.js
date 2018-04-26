999fc53fecd9639e5bdb4b5d47f98d3b9ebd0889
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-04-26 14:05:06 +0200

    Better scroll handling

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: "dummy",
     environment,
     rootURL: "/",
-    locationType: "auto",
+    locationType: "router-scroll",
     i18n: {
       defaultLocale: "fr"
     },
