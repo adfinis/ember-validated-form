cff65c89c62141913333894c20b2da805a096aa8
yelinz <yelin.zhang@adfinis.com>
2021-01-27 16:27:25 +0100

    fix: fix linting issues

--- tests/integration/components/validated-input/types/select-test.js
+++ tests/integration/components/validated-input/types/select-test.js
@@ -3,20 +3,21 @@ import { setupRenderingTest } from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
 import { module, test } from "qunit";
 
-module("Integration | Component | validated-input/types/select", function (
-  hooks
-) {
-  setupRenderingTest(hooks);
+module(
+  "Integration | Component | validated-input/types/select",
+  function (hooks) {
+    setupRenderingTest(hooks);
 
-  test("it renders", async function (assert) {
-    this.set("options", [
-      { key: 1, label: 1 },
-      { key: 2, label: 2 },
-    ]);
+    test("it renders", async function (assert) {
+      this.set("options", [
+        { key: 1, label: 1 },
+        { key: 2, label: 2 },
+      ]);
 
-    await render(hbs`{{validated-input/types/select options=options}}`);
+      await render(hbs`{{validated-input/types/select options=options}}`);
 
-    assert.dom("select").exists();
-    assert.dom("option").exists({ count: 2 });
-  });
-});
+      assert.dom("select").exists();
+      assert.dom("option").exists({ count: 2 });
+    });
+  }
+);
--- tests/integration/components/validated-input/types/textarea-test.js
+++ tests/integration/components/validated-input/types/textarea-test.js
@@ -3,14 +3,15 @@ import { setupRenderingTest } from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
 import { module, test } from "qunit";
 
-module("Integration | Component | validated-input/types/textarea", function (
-  hooks
-) {
-  setupRenderingTest(hooks);
+module(
+  "Integration | Component | validated-input/types/textarea",
+  function (hooks) {
+    setupRenderingTest(hooks);
 
-  test("it renders", async function (assert) {
-    await render(hbs`{{validated-input/types/textarea}}`);
+    test("it renders", async function (assert) {
+      await render(hbs`{{validated-input/types/textarea}}`);
 
-    assert.dom("textarea").exists();
-  });
-});
+      assert.dom("textarea").exists();
+    });
+  }
+);
