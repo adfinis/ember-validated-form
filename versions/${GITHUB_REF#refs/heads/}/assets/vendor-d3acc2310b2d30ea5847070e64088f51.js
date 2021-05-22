461c40cdb8c15901f010b45c000959cc06ed48ba
Tomster <tomster@emberjs.com>
2021-05-22 02:24:12 +0000

    Deployed 5867d73

--- versions/${GITHUB_REF#refs/heads/}/assets/vendor-d3acc2310b2d30ea5847070e64088f51.js
+++ versions/${GITHUB_REF#refs/heads/}/assets/vendor-d3acc2310b2d30ea5847070e64088f51.js
@@ -13532,13 +13532,13 @@ var n=Array.isArray(t),i=Array.isArray(e)
 if(n===i)return n||null==e?t:Y(e,t,r)
 var o=N(t)
 return i&&o?I(Y(L(e),t,r)):t}var J=function(){return(J=Object.assign||function(e){for(var t,r=1,n=arguments.length;r<n;r++)for(var i in t=arguments[r])Object.prototype.hasOwnProperty.call(t,i)&&(e[i]=t[i])
-return e}).apply(this,arguments)},X={get:function(e,t){var r
-if(!("symbol"===n(t)||e.changes.hasOwnProperty&&e.changes.hasOwnProperty(t)&&void 0===(r=e.safeGet(e.changes,t)))){if(s(r))return c(r)
-if(i(r)){var o=e.children[t]
-if(void 0===o&&e.content){var a=e.safeGet(e.content,t)
-o=e.children[t]=new te(r,a,e.safeGet)}if(o)return o.proxy}if(void 0!==r)return r
-if(e.content){var l=e.content
-if(e.safeGet(l,t))return e.safeGet(l,t)}return"function"==typeof e[t]||e.hasOwnProperty(t)?e[t]:void 0}},ownKeys:function(e){return Reflect.ownKeys(e.changes)},getOwnPropertyDescriptor:function(e,t){return Reflect.getOwnPropertyDescriptor(e.changes,t)},has:function(e,t){return Reflect.has(e.changes,t)},set:function(e,t,r){return t.startsWith("_")?Reflect.set(e,t,r):Reflect.set(e.changes,t,new a(r))}}
+return e}).apply(this,arguments)},X={get:function(e,t){if("symbol"!==n(t)){var r,o=e.safeGet(e.changes,t)
+if(o){if(s(o))return c(o)
+r=o}if(i(r)){var a=e.children[t]
+if(void 0===a&&e.content){var l=e.safeGet(e.content,t)
+a=e.children[t]=new te(r,l,e.safeGet)}if(a)return a.proxy}if(void 0!==r)return r
+if(e.content){var u=e.content
+if(e.safeGet(u,t))return e.safeGet(u,t)}return"function"==typeof e[t]||e.hasOwnProperty(t)?e[t]:void 0}},ownKeys:function(e){return Reflect.ownKeys(e.changes)},getOwnPropertyDescriptor:function(e,t){return Reflect.getOwnPropertyDescriptor(e.changes,t)},has:function(e,t){return Reflect.has(e.changes,t)},set:function(e,t,r){return t.startsWith("_")?Reflect.set(e,t,r):Reflect.set(e.changes,t,new a(r))}}
 function ee(e,t){return e[t]}var te=function(){function e(e,t,r,n){void 0===e&&(e={}),void 0===t&&(t={}),void 0===r&&(r=ee),void 0===n&&(n=n),this.safeGet=r,this.isObject=n,this.changes=e,this.content=t,this.proxy=new Proxy(this,X),this.children=Object.create(null)}return e.prototype.get=function(e){return this.safeGet(this.changes,e)},e.prototype.set=function(e,t){return F(this.changes,e,t)},e.prototype.unwrap=function(){var e=this.changes
 if(i(e)){e=M(e,this.isObject)
 var t=this.content
