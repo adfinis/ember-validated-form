7641b69dde703598643af6d6e0958b80a3feee35
Christian Zosel <christian@zosel.ch>
2021-07-13 15:59:22 +0200

    fix: call on-update hook correctly
    This fixes a regression introduced with the update to glimmer.

--- testem.js
+++ testem.js
@@ -4,7 +4,7 @@ module.exports = {
   test_page: "tests/index.html?hidepassed",
   disable_watching: true,
   launch_in_ci: ["Chrome"],
-  launch_in_dev: ["Chrome"],
+  launch_in_dev: [],
   browser_start_timeout: 120,
   browser_args: {
     Chrome: {
