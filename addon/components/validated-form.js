9266f56378495e13ababa71531b407c1949a8a43
Staffan Edström <staffanedstrom@gmail.com>
2023-02-23 13:23:35 +0000

    fix: lint

--- addon/components/validated-form.js
+++ addon/components/validated-form.js
@@ -48,7 +48,9 @@ export default class ValidatedFormComponent extends Component {
       if (macroCondition(getOwnConfig().scrollErrorIntoView)) {
         if (model.errors[0]?.key) {
           document
-            .querySelector(`[name=${model.errors[0].key.replaceAll(".", "\\.")}]`)
+            .querySelector(
+              `[name=${model.errors[0].key.replaceAll(".", "\\.")}]`
+            )
             ?.scrollIntoView({ behavior: "smooth" });
         }
       }
